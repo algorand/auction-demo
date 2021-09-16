68ff68e58409fd584212514d65b87791aaca2374
Jason Paulos <jasonpaulos@users.noreply.github.com>
2021-09-16 14:21:21 -0700

    Start writing contract

---
+++ contracts/auction.py
@@ -0,0 +1,71 @@
+from pyteal import *
+
+
+def approval_program():
+    seller_key = Bytes("seller")
+    nft_id_key = Bytes("nft_id")
+    start_time_key = Bytes("start")
+    end_time_key = Bytes("end")
+    reserve_amount_key = Bytes("reserve_amount")
+    min_bid_increment_key = Bytes("min_bid_inc")
+    num_bids_key = Bytes("num_bids")
+    lead_bid_amount_key = Bytes("bid_amount")
+    lead_bid_account_key = Bytes("bid_account")
+
+    on_create_start_time = Btoi(Txn.application_args[2])
+    on_create_end_time = Btoi(Txn.application_args[3])
+    on_create = Seq(
+        App.globalPut(seller_key, Txn.application_args[0]),
+        App.globalPut(nft_id_key, Btoi(Txn.application_args[1])),
+        App.globalPut(start_time_key, on_create_start_time),
+        App.globalPut(end_time_key, on_create_end_time),
+        App.globalPut(reserve_amount_key, Btoi(Txn.application_args[4])),
+        App.globalPut(min_bid_increment_key, Btoi(Txn.application_args[5])),
+        Assert(And(
+            Global.latest_timestamp() < on_create_start_time,
+            on_create_start_time < on_create_end_time
+        )),
+        Approve()
+    )
+
+    on_call = Seq(
+        # TODO: implement auction operations
+        Approve()
+    )
+
+    on_delete = Seq(
+        # TODO: only allow delete if auction has finished
+        Reject()
+    )
+
+    program = Cond(
+        [Txn.application_id() == Int(0), on_create],
+        [Txn.on_completion() == OnComplete.NoOp, on_call],
+        [
+            Txn.on_completion() == OnComplete.DeleteApplication,
+            on_delete,
+        ],
+        [
+            Or(
+                Txn.on_completion() == OnComplete.OptIn,
+                Txn.on_completion() == OnComplete.CloseOut,
+                Txn.on_completion() == OnComplete.UpdateApplication,
+            ),
+            Reject(),
+        ],
+    )
+
+    return program
+
+
+def clear_state_program():
+    return Approve()
+
+if __name__ == "__main__":
+    with open("auction_approval.teal", "w") as f:
+        compiled = compileTeal(approval_program(), mode=Mode.Application, version=5)
+        f.write(compiled)
+
+    with open("auction_clear_state.teal", "w") as f:
+        compiled = compileTeal(clear_state_program(), mode=Mode.Application, version=5)
+        f.write(compiled)
