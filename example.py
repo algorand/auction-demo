407db6a5c0f90a47680c2fca5aaed26dc4592e8d
Jason Paulos <jasonpaulos@users.noreply.github.com>
2021-11-28 23:58:00 -0500

    Improve example script messages

--- example.py
+++ example.py
@@ -19,22 +19,26 @@ from auction.testing.resources import (
 def simple_auction():
     client = getAlgodClient()
 
-    print("Alice is generating temporary accounts...")
+    print("Generating temporary accounts...")
     creator = getTemporaryAccount(client)
     seller = getTemporaryAccount(client)
+    bidder = getTemporaryAccount(client)
+
+    print("Alice (seller account):", seller.getAddress())
+    print("Bob (auction creator account):", creator.getAddress())
+    print("Carla (bidder account)", bidder.getAddress(), "\n")
 
     print("Alice is generating an example NFT...")
     nftAmount = 1
     nftID = createDummyAsset(client, nftAmount, seller)
-    print("The NFT ID is:", nftID)
+    print("The NFT ID is", nftID)
+    print("Alice's balances:", getBalances(client, seller.getAddress()), "\n")
+
     startTime = int(time()) + 10  # start time is 10 seconds in the future
     endTime = startTime + 30  # end time is 30 seconds after start
     reserve = 1_000_000  # 1 Algo
     increment = 100_000  # 0.1 Algo
-
-    print(
-        "Alice is creating auction smart contract that lasts 30 seconds to auction off NFT..."
-    )
+    print("Bob is creating an auction that lasts 30 seconds to auction off the NFT...")
     appID = createAuctionApp(
         client=client,
         sender=creator,
@@ -45,6 +49,14 @@ def simple_auction():
         reserve=reserve,
         minBidIncrement=increment,
     )
+    print(
+        "Done. The auction app ID is",
+        appID,
+        "and the escrow account is",
+        get_application_address(appID),
+        "\n",
+    )
+
     print("Alice is setting up and funding NFT auction...")
     setupAuctionApp(
         client=client,
@@ -54,45 +66,47 @@ def simple_auction():
         nftID=nftID,
         nftAmount=nftAmount,
     )
+    print("Done\n")
 
-    sellerAlgosBefore = getBalances(client, seller.getAddress())[0]
-
-    print("Alice's algo balance: ", sellerAlgosBefore, " algos")
-
-    bidder = getTemporaryAccount(client)
+    sellerBalancesBefore = getBalances(client, seller.getAddress())
+    sellerAlgosBefore = sellerBalancesBefore[0]
+    print("Alice's balances:", sellerBalancesBefore)
 
     _, lastRoundTime = getLastBlockTimestamp(client)
     if lastRoundTime < startTime + 5:
         sleep(startTime + 5 - lastRoundTime)
     actualAppBalancesBefore = getBalances(client, get_application_address(appID))
-    print("The smart contract now holds the following:", actualAppBalancesBefore)
+    print("Auction escrow balances:", actualAppBalancesBefore, "\n")
+
     bidAmount = reserve
-    bidderAlgosBefore = getBalances(client, bidder.getAddress())[0]
-    print("Carla wants to bid on NFT, her algo balance: ", bidderAlgosBefore, " algos")
-    print("Carla is placing bid for: ", bidAmount, " algos")
+    bidderBalancesBefore = getBalances(client, bidder.getAddress())
+    bidderAlgosBefore = bidderBalancesBefore[0]
+    print("Carla wants to bid on NFT, her balances:", bidderBalancesBefore)
+    print("Carla is placing bid for", bidAmount, "microAlgos")
 
     placeBid(client=client, appID=appID, bidder=bidder, bidAmount=bidAmount)
 
-    print("Carla is opting into NFT with id:", nftID)
+    print("Carla is opting into NFT with ID", nftID)
 
     optInToAsset(client, nftID, bidder)
 
+    print("Done\n")
+
     _, lastRoundTime = getLastBlockTimestamp(client)
     if lastRoundTime < endTime + 5:
-        sleep(endTime + 5 - lastRoundTime)
+        waitTime = endTime + 5 - lastRoundTime
+        print("Waiting {} seconds for the auction to finish\n".format(waitTime))
+        sleep(waitTime)
 
-    print("Alice is closing out the auction....")
+    print("Alice is closing out the auction\n")
     closeAuction(client, appID, seller)
 
     actualAppBalances = getBalances(client, get_application_address(appID))
     expectedAppBalances = {0: 0}
-    print("The smart contract now holds the following:", actualAppBalances)
+    print("The auction escrow now holds the following:", actualAppBalances)
     assert actualAppBalances == expectedAppBalances
 
     bidderNftBalance = getBalances(client, bidder.getAddress())[nftID]
-
-    print("Carla's NFT balance:", bidderNftBalance, " for NFT ID: ", nftID)
-
     assert bidderNftBalance == nftAmount
 
     actualSellerBalances = getBalances(client, seller.getAddress())
